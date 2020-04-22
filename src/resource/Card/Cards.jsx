eddc1d33ca60fc582df550b45279ca5f7cc39283
Ashu <ashenshi@gmail.com>
2020-04-22 12:51:33 +0200

    data fetched on Cards.jsx

--- src/resource/Card/Cards.jsx
+++ src/resource/Card/Cards.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import styles from './Card.module.css';
+import cx from 'classnames';//for multiple styling
 
-const Cards = (props) => {
-    console.log(props)
+/* const Cards = (props) => {
+    console.log(props) to fetch data & show it on cards.jsx do the below*/
+    const Cards = ({data: {confirmed, recovered, deaths, dailyTimeSeries} }) => {
+        if(!confirmed){
+            return 'Searching ...'
+        }
     return(
         <div className={styles.container}>
-          {/*   <div class="row"> */}
+          <div className={styles.card}> 
             <div class="col s12 m6">
             <div class="card blue-grey darken-1">
             <div class="card-content white-text">
-             <p>Infected</p>
-            <p>Data here</p>
+             <p>Confirmed-Infection</p>
+            <p>{confirmed.value}</p> 
             <p>Date here</p>
             <p>Covid-19 Pandemic Current Information</p>
                 </div>
@@ -20,7 +25,38 @@ const Cards = (props) => {
              </div>
              </div>
             </div>
-        {/*  </div> */}
+        </div>
+
+        {/* Recovered */}
+        <div class="col s12 m6">
+            <div class="card blue-grey darken-1">
+            <div class="card-content white-text">
+             <p>Recovered-Patients</p>
+            <p>{recovered.value}</p> 
+            <p>Date here</p>
+            <p>Covid-19 Pandemic Current Information</p>
+                </div>
+            <div class="card-action">
+            <a href="https://www.who.int/">Link to WHO</a>
+           
+             </div>
+             </div>
+            </div>
+            {/* deaths */}
+            <div class="col s12 m6">
+            <div class="card blue-grey darken-1">
+            <div class="card-content white-text">
+             <p>Confirmed-Deaths</p>
+            <p>{deaths.value}</p> 
+            <p>Date here</p>
+            <p>Covid-19 Pandemic Current Information</p>
+                </div>
+            <div class="card-action">
+            <a href="https://www.who.int/">Link to WHO</a>
+           
+             </div>
+             </div>
+            </div>
         </div>
     )
 }
