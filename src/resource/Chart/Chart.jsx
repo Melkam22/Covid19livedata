47c58676aea3a9c702e3cc03c3627f018ba08759
Ashu <ashenshi@gmail.com>
2020-04-23 19:19:19 +0200

    line-chart shown but not all datas shown

--- src/resource/Chart/Chart.jsx
+++ src/resource/Chart/Chart.jsx
@@ -21,9 +21,11 @@ const showLineChart = (
             //datasets: [{},{}]before the data was mapped
             labels: Ddata.map(({date})=> date),
             datasets: [
-                { data: Ddata.map(({confirmed})=>confirmed)},
-                {data: Ddata.map(({deaths})=>deaths)}
-                    ]
+                //{ data: Ddata.map(({confirmed})=>confirmed)}before,
+                //{data: Ddata.map(({deaths})=>deaths)}
+                { data: Ddata.map(({confirmed})=>confirmed), label: 'Infected', borderColor:'rgba(255,0,0,1)', fill: true},
+                {data: Ddata.map(({deaths})=>deaths), label: 'Deaths', borderColor: 'rgba(0,0,255,1)', backgroundColor:'rgba(0,0,255,.4)', fill: true},
+                    ],
         }}
     /> ) : null
 )
